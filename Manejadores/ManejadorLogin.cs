4779bbed6d4866fc8b5c7e4684c065ebd26ab08b
Ulises E.G. <escobar.gonzalez.ulises@gmail.com>
2025-11-17 15:27:19 -0600

    Creacion de Login y formulario de menu (prueba)

--- Manejadores/ManejadorLogin.cs
+++ Manejadores/ManejadorLogin.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using AccesoDatos;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Manejadores
 {
     public class ManejadorLogin
     {
+        Base b = new Base("localhost", "root", "12345", "CarpinteriaDB");
         public static string Sha1(string texto)
         {
             SHA1 sha = SHA1.Create();
@@ -20,6 +24,13 @@ namespace Manejadores
             }
             return cadena.ToString();
         }
-
+        public bool Validar(TextBox usuario, TextBox clave)
+        {
+            DataRow dr = b.Consultar($"call p_validar('{usuario.Text}', '{Sha1(clave.Text)}')", "Usuarios").Tables[0].Rows[0];
+            if (dr["rs"].ToString().Equals("Aceptado"))
+                return true;
+            else
+                return false;
+        }
     }
 }
